b326e10d11a219ae622f1309f1d08615a5da3820
Lila <Lila.helminiak@gmail.com>
2014-11-09 23:00:23 +0100

    User and Admin authentication is working

---
+++ PeopleApp/app/helpers/people_app_helper.rb
@@ -0,0 +1,2 @@
+module PeopleAppHelper
+end
---
+++ PeopleApp/db/migrate/20141108001658_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :mail
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
